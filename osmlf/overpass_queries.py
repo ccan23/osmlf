d984ca0be5308bc1ae8eca3182b5ea59083d6227
ccanb <dev.ccanb@protonmail.com>
2023-06-18 21:37:56 +0300

    Modified the `generate_osm_query` function to accept a list of values instead of a single value. The Overpass QL query now includes way and relation objects that match the given key with any of the specified values, improving the flexibility and usefulness of data retrieval.

--- osmlf/overpass_queries.py
+++ osmlf/overpass_queries.py
@@ -13,26 +13,29 @@ class queries:
         out geom;
         """
     
-    def generate_osm_query(osm_id: int, key: str) -> str:
+    def generate_osm_query(osm_id: int, key: str, values: list) -> str:
         """
-        Given an OpenStreetMap ID, a key, and a value, generate an Overpass QL query that retrieves 
-        related way and relation objects from the OpenStreetMap database.
+        Given an OpenStreetMap ID, a key, and a list of values, generate an Overpass QL query that retrieves 
+        related way and relation objects from the OpenStreetMap database for any of the given values.
         
         Args:
             osm_id (int): The OpenStreetMap ID to base the query on.
             key (str): The key to use in the Overpass QL query.
-            value (str): The value to match with the key in the Overpass QL query.
+            values (list): The values to match with the key in the Overpass QL query.
 
         Returns:
             str: A string that represents an Overpass QL query.
         """
+        # Create the string representing the list of possible key-value matches
+        value_string = ''.join([f'way(area.a)["{key}"="{value}"];relation(area.a)["{key}"="{value}"];' for value in values])
+        
+        # Insert the value string into the Overpass QL query
         return f"""
         [out:json];
         rel({osm_id});
         map_to_area->.a;
         (
-        way(area.a)["{key}"];
-        relation(area.a)["{key}"];
+        {value_string}
         );
         out body;
         >;
