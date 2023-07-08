56cccf28683a6d8448a6ec805d34d3d76db07da1
ccan23 <dev.ccanb@protonmail.com>
2023-07-08 15:37:52 -0400

    Minor fixes

--- osmlf/overpass_calculations.py
+++ osmlf/overpass_calculations.py
@@ -66,7 +66,7 @@ class calculations:
             # Store the way's ID, name, original coordinates
             way_features['ways'].append({
                 'way_id'     : way.id,
-                'name'       : way.tags.get('name', 'unknown'),
+                'tags'       : way.tags,
                 'coordinates': coordinates,
                 'area'       : area
             })
@@ -76,39 +76,6 @@ class calculations:
         way_features['total_area'] = sum([way['area'] for way in way_features['ways']])
 
         return way_features
-    
-    def area_of_members(members: list, utm_zone: str) -> dict:
-        """
-        Compute the total area of all geometries in the given OSM relation members in a specific UTM zone.
-        
-        This function first extracts all coordinates from the provided members. 
-        It then uses the Transformer from pyproj to convert these coordinates from the WGS84 format to the given UTM zone.
-        It forms a polygon from these converted coordinates and calculates its area in square kilometers.
-        
-        Args:
-            members (list): A list of OSM relation members. Each member should have 'geometry' which should contain 'lon' and 'lat'.
-            utm_zone (str): The UTM zone to be used for the area calculation. This should be a string in the format expected by pyproj.
-
-        Returns:
-            float: The total area of all the geometries, in square kilometers.
-        """
-    
-        # Initialize a Transformer object for converting the coordinates from WGS84 to the specified UTM zone
-        transformer = Transformer.from_crs('EPSG:4326', utm_zone, always_xy=True)
-
-        # Extract all coordinates from 'outer' member geometries
-        # Each tuple contains a longitude and latitude pair
-        coordinates = [(float(geometry.lon), float(geometry.lat)) for member in members for geometry in member.geometry]
-
-        # Convert the extracted WGS84 coordinates to the specified UTM zone using the transformer
-        coordinates_projected = [transformer.transform(*coord) for coord in coordinates]
-
-        # Create a Polygon using the projected coordinates
-        polygon_projected = Polygon(coordinates_projected)
-
-        # Compute the area of the created polygon and convert it to square kilometers (since the original area is in square meters)
-        # Return the computed area
-        return polygon_projected.area / 10**6 
 
     def total_distance(coordinates, utm_zone) -> float:
         """
@@ -140,3 +107,36 @@ class calculations:
             total_distance += distance
 
         return total_distance
+    
+    def area_of_members(members: list, utm_zone: str) -> dict:
+        """
+        Compute the total area of all geometries in the given OSM relation members in a specific UTM zone.
+        
+        This function first extracts all coordinates from the provided members. 
+        It then uses the Transformer from pyproj to convert these coordinates from the WGS84 format to the given UTM zone.
+        It forms a polygon from these converted coordinates and calculates its area in square kilometers.
+        
+        Args:
+            members (list): A list of OSM relation members. Each member should have 'geometry' which should contain 'lon' and 'lat'.
+            utm_zone (str): The UTM zone to be used for the area calculation. This should be a string in the format expected by pyproj.
+
+        Returns:
+            float: The total area of all the geometries, in square kilometers.
+        """
+
+        # Initialize a Transformer object for converting the coordinates from WGS84 to the specified UTM zone
+        transformer = Transformer.from_crs('EPSG:4326', utm_zone, always_xy=True)
+
+        # Extract all coordinates from 'outer' member geometries
+        # Each tuple contains a longitude and latitude pair
+        coordinates = [(float(geometry.lon), float(geometry.lat)) for member in members for geometry in member.geometry]
+
+        # Convert the extracted WGS84 coordinates to the specified UTM zone using the transformer
+        coordinates_projected = [transformer.transform(*coord) for coord in coordinates]
+
+        # Create a Polygon using the projected coordinates
+        polygon_projected = Polygon(coordinates_projected)
+
+        # Compute the area of the created polygon and convert it to square kilometers (since the original area is in square meters)
+        # Return the computed area
+        return polygon_projected.area / 10**6
